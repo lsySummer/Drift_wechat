e33a6159fea8d0883d341124d0b9f18c2d84e2a5
hannatao <413024870@qq.com>
2017-10-20 14:19:16 +0800

    按钮小问题测试

--- src/main/webapp/js/myJS/Delivery.js
+++ src/main/webapp/js/myJS/Delivery.js
@@ -7,14 +7,16 @@ $.getJSON('/Drift_wechat/api/delivery/get',function(json){
 		document.getElementById("container").innerHTML="暂无订单";
 	}else{
 		if(json.enable != "上家已发货"){
-			$('#confirm').attr('disabled',"true");
+//			$('#confirm').attr('disabled',"true");
+			document.getElementById('confirm').disabled="true";
 		}
 		document.getElementById('previous').innerHTML=json.before;
 		document.getElementById('deliveryNum1').innerHTML=json.receive;
 		document.getElementById('deviceId1').innerHTML=json.deviceId;
 		if(json.after != null){
 			if(json.enable != "已收货"){
-				$('#detail').attr('disabled',"true");
+//				$('#detail').attr('disabled',"true");
+				document.getElementById('detail').disabled="true";
 			}
 			document.getElementById('next').innerHTML=json.after;
 			document.getElementById('deliveryNum2').innerHTML=json.send;
