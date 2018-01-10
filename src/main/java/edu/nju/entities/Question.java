e0043888abeed6cc6c9d63f849c3e0572d9925d4
lsy <lsy@lsydeMacBook-Pro.local>
2018-01-10 10:43:52 +0800

    修改字段名

--- src/main/java/edu/nju/entities/Question.java
+++ src/main/java/edu/nju/entities/Question.java
@@ -22,7 +22,7 @@ public class Question {
 	private String title;
 	private String content;
 	private Date createTime;
-	private int signal;//图片标志位，0代表没有图片，1代表有图片
+	private int picSig;//图片标志位，0代表没有图片，1代表有图片
 	@Id
 	@Column(length = 32)
 	@GeneratedValue(generator = "system-uuid")
@@ -57,11 +57,11 @@ public class Question {
 	public void setCreateTime(Date createTime) {
 		this.createTime = createTime;
 	}
-	public int getSignal() {
-		return signal;
+	public int getPicSig() {
+		return picSig;
 	}
-	public void setSignal(int signal) {
-		this.signal = signal;
+	public void setPicSig(int picSig) {
+		this.picSig = picSig;
 	}
 	
 }
